bf35a7d59b1571301d5deea557e7a6944fe52f8f
Thabo Chauke <chaukethabo16@gmail.com>
2024-09-23 01:03:15 +0200

    remove tab button and related js

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js"
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-database.js"
+
+
+const firebaseConfig = {
+}
+const app = initializeApp(firebaseConfig)
+const database = getDatabase(app)
+
 const saveBtn = document.getElementById("input-btn")
 const deleteBtn = document.getElementById("delete-btn")
-const tabBtn = document.getElementById("tab-btn")
 
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
@@ -37,12 +45,4 @@ deleteBtn.addEventListener("dblclick", function() {
     myLeads = []
     localStorage.clear()
     render(myLeads)
-})
-
-tabBtn.addEventListener("click", function() {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        myLeads.push(tabs[0].url)
-        localStorage.setItem("myLeads", JSON.stringify(myLeads))
-        render(myLeads)
-    })
 })
