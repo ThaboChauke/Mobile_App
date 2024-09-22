a4a083ac8023dab02df02bc3a8a9fd39604a3060
Thabo Chauke <chaukethabo16@gmail.com>
2024-09-23 01:05:29 +0200

    remove local storage

--- script.js
+++ script.js
@@ -10,18 +10,11 @@ const database = getDatabase(app)
 const saveBtn = document.getElementById("input-btn")
 const deleteBtn = document.getElementById("delete-btn")
 
-const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
-
 const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
 
 let myLeads = []
 
-
-if (leadsFromLocalStorage) {
-    myLeads = leadsFromLocalStorage
-    render(myLeads)
-}
 
 function render(array) {
     let listItems = ""
@@ -37,12 +30,10 @@ function render(array) {
 saveBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
     render(myLeads)
 })
 
 deleteBtn.addEventListener("dblclick", function() {
     myLeads = []
-    localStorage.clear()
     render(myLeads)
 })
